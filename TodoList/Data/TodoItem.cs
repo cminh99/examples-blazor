b506ff4672b069977f7b3f9a59c579b9ac1d3fbf
cminh99 <nhcminh99@gmail.com>
2021-01-11 16:47:00 +0700

    TodoList

---
+++ TodoList/Data/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Data
+{
+    public class TodoItem
+    {
+        public bool isDone { get; set; }
+        public string title { get; set; }
+    }
+}
---
+++ TodoList/Data/TodoItemServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TodoList.Data
+{
+    public class TodoItemServices
+    {
+        private List<TodoItem> todos;
+        private string _file = "Data\\todo.json";
+
+        public List<TodoItem> GetTodos()
+        {
+            if (File.Exists(_file))
+            {
+                using var file = File.OpenText(_file);
+                var serializer = new JsonSerializer();
+                todos = serializer.Deserialize(file, typeof(List<TodoItem>)) as List<TodoItem>;
+            }
+            return todos;
+        }
+
+        public void SaveChanges()
+        {
+            using var file = File.CreateText(_file);
+            var serializer = new JsonSerializer();
+            serializer.Serialize(file, todos);
+        }
+    }
+}
