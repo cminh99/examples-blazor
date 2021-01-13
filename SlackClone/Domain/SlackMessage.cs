490ffa15b54ae2cc9f6b02e243e5f1c6938f4508
cminh99 <nhcminh99@gmail.com>
2021-01-13 14:40:50 +0700

    Hoàn thành SlackClone

---
+++ SlackClone/Domain/SlackMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlackClone.Domain
+{
+    public class SlackMessage
+    {
+        public DateTime PostedOn { get; set; }
+        public string PostedBy { get; set; }
+        public string Message { get; set; }
+        public int Like { get; set; }
+    }
+}
